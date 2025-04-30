1790fa93111c545f2201cae0bf362fbe4b95c7fc
Mirwes Hakimi <mirwes215@gmail.com>
2025-04-29 20:33:03 -0700

    created test to renders all headings, buttons and static texts

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,27 @@
 // Write your tests here
+import React from 'react';
+import { render, waitFor, screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import AppFunctional from './AppFunctional';
+
+// describe('App Functional component', () => {
+//   /// mock API setup
+//   beforeAll(() => {server.listen() })
+//   afterAll(() => { server.close() })
+
+//   let userInput, 
+// })
+
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true); // This will pass
+});
+
+/// renders all headings, buttons and static text
+test('renders heading and buttons', () => {
+  render(<AppFunctional/>)
+  expect(screen.getByText(/coordinates/i)).toBeInTheDocument()
+  expect(screen.getByRole('button', { name: /left/i })).toBeInTheDocument();
+  expect(screen.getByRole('button', { name: /right/i })).toBeInTheDocument()
+  expect(screen.getByRole('button', { name: /reset/i })).toBeInTheDocument()
 })
